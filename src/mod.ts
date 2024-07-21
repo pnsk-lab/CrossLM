37e0a1cca73ee58447d7c9dfaab12118e6c47e71
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-07-21 07:28:59 +0000

    chore: add example for module doc

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,20 @@
 /**
- * CrossLM entry point
+ * @example
+ * ```ts
+ * import { CrossLM } from '@pnsk-lab/crosslm'
+ * import { CommandRPlus } from '@pnsk-lab/crosslm/cohere/command-r-plus'
+ *
+ * const lm = new CrossLM([new CommandRPlus()])
+ * 
+ * const { end, stream } = lm.generateStream([], {
+ *   messages: [{ role: 'system', text: '1+1=?' }]
+ * })
+ * 
+ * for await (const { text } of stream) {
+ *   console.log(text)
+ * }
+ * ```
  * @module
  */
-
 export { CrossLM } from './crosslm.ts'
 export * from './types.ts'
